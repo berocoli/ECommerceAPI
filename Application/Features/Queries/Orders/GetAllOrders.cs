236f12049cfd964276c836f61b73d7332443b780
Berke Öztürk <berkeozturk3002@gmail.com>
2024-08-27 17:59:43 +0300

    MediatR Setups
    W.I.P.

---
+++ Application/Features/Queries/Orders/GetAllOrders.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Application.Features.Queries.Orders
+{
+    public class GetAllOrders
+    {
+        public GetAllOrders()
+        {
+        }
+    }
+}
+
---
+++ Application/Features/Queries/Users/GetAllUsers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Application.Features.Queries.Users
+{
+    public class GetAllUsers
+    {
+        public GetAllUsers()
+        {
+        }
+    }
+}
+
